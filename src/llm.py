d7bb3e356ff454678b6ae8a9e59f233b1f9e77d0
Latheesh Poondla <latheeshpoondla1@gmail.com>
2026-04-29 13:37:57 +0530

    modification for llama3.1 and desktop app

--- src/llm.py
+++ src/llm.py
@@ -22,3 +22,46 @@ def ask_llm(context, question):
     )
 
     return response.json()["response"]
+
+# # Code for Meta llama3.1 8B Intruct hugging-face
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+# import torch
+
+# MODEL_PATH = r"D:\Latheesh\Projects\dsmlai_projects\intelAssist\models\Meta-Llama3.1-8B-Instruct"
+
+# # Load once (IMPORTANT)
+# tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+# model = AutoModelForCausalLM.from_pretrained(
+#     MODEL_PATH,
+#     device_map="auto",   # CPU/GPU auto
+#     dtype=torch.float32
+# )
+
+# device = torch.device("cpu")
+# model.to(device)
+
+# def ask_llm(context, question):
+    
+#     prompt = f"""
+# You are an intelligent assistant.
+# Answer ONLY from the given context.
+
+# Context:
+# {context}
+
+# Question:
+# {question}
+# """
+
+#     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+
+#     with torch.no_grad():
+#         outputs = model.generate(
+#             **inputs,
+#             max_new_tokens=200,
+#             temperature=0.7,
+#             do_sample=True
+
+#         )
+
+#     return tokenizer.decode(outputs[0], skip_special_tokens=True)
